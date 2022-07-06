1041c7f624aefa8c889151a8fa08b36d444919e2
mahimaupadhya <62554816+mahimaupadhya@users.noreply.github.com>
2022-07-06 21:22:08 +0530

    Update LearningresourcesApiApplication.java

--- src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-pubgilic class LearningresourcesApiApplication{
+public class LearningresourcesApiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesApiApplication.class, args);
