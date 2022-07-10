5340d158261c537d91f8de35edc8bf3063e641ee
mahimaupadhya <mahimaupadhya115@gmail.com>
2022-07-10 18:28:07 +0530

    Add method to save data to csv and also sort by profit margin

--- src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -1,14 +1,21 @@
 package com.tgt.rysetii.learningresourcesapi;
 
+import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
+import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
+import static java.util.Collections.unmodifiableList;
+
 @SpringBootApplication
-public class LearningresourcesApiApplication{
+public class LearningresourcesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesApiApplication.class, args);
 
-	}
 
+
+	}
 }
